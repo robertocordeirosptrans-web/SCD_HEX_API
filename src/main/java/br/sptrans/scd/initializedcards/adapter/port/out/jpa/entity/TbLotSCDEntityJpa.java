fb51833227993b40ffca25906691845ef30a806b
robertocordeirosptrans-web <robertocordeiro.sptrans@gmail.com>
2026-03-17 16:35:09 -0300

    feat: Add missing @Id annotation to TbLotSCDEntityJpa class

--- src/main/java/br/sptrans/scd/initializedcards/adapter/port/out/jpa/entity/TbLotSCDEntityJpa.java
+++ src/main/java/br/sptrans/scd/initializedcards/adapter/port/out/jpa/entity/TbLotSCDEntityJpa.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,7 +18,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TbLotSCDEntityJpa {
-
+    @Id
     @Column(name = "ID_LOTE")
     private Long idLote;
 
