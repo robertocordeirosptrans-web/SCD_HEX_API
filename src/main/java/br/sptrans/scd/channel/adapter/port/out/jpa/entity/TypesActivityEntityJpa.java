c707580a61e8b2ec6e59a41e7d3f7c3bfcd3b6a9
robertocordeirosptrans-web <robertocordeiro.sptrans@gmail.com>
2026-03-20 16:56:32 -0300

    feat: Incluindo Schema

--- src/main/java/br/sptrans/scd/channel/adapter/port/out/jpa/entity/TypesActivityEntityJpa.java
+++ src/main/java/br/sptrans/scd/channel/adapter/port/out/jpa/entity/TypesActivityEntityJpa.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "TIPOS_ATIVIDADE")
+@Table(name = "TIPOS_ATIVIDADE", schema="SPTRANSDBA")
 @Getter
 @Setter
 @NoArgsConstructor
