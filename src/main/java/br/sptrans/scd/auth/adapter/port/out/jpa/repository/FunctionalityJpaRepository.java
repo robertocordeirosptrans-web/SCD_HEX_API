0093c36a0d3d3a6a4c16ed4f87dd491ca9f459ca
robertocordeirosptrans-web <robertocordeiro.sptrans@gmail.com>
2026-03-20 15:48:18 -0300

    feat: Adicionando Consulta de Funcionalidades Ativas

--- src/main/java/br/sptrans/scd/auth/adapter/port/out/jpa/repository/FunctionalityJpaRepository.java
+++ src/main/java/br/sptrans/scd/auth/adapter/port/out/jpa/repository/FunctionalityJpaRepository.java
@@ -1,11 +1,22 @@
+
+
 package br.sptrans.scd.auth.adapter.port.out.jpa.repository;
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+
 import br.sptrans.scd.auth.adapter.port.out.jpa.entity.FunctionalityEntityJpa;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
 import br.sptrans.scd.auth.adapter.port.out.jpa.entity.FunctionalityEntityJpaKey;
 
 public interface FunctionalityJpaRepository extends JpaRepository<FunctionalityEntityJpa, FunctionalityEntityJpaKey>, JpaSpecificationExecutor<FunctionalityEntityJpa> {
-    
+
+    @Query("SELECT f FROM FunctionalityEntityJpa f WHERE f.codStatus = 'A'")
+    List<FunctionalityEntityJpa> findAllActive();
 }
