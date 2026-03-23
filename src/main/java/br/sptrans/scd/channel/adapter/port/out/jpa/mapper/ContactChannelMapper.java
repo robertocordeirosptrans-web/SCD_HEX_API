d53b32dd4a91d721b2c8eb66670176076475e967
robertocordeirosptrans-web <robertocordeiro.sptrans@gmail.com>
2026-03-23 10:05:15 -0300

    feat: Implementação de mapper para Contatos

--- src/main/java/br/sptrans/scd/channel/adapter/port/out/jpa/mapper/ContactChannelMapper.java
+++ src/main/java/br/sptrans/scd/channel/adapter/port/out/jpa/mapper/ContactChannelMapper.java
@@ -1,21 +1,50 @@
+
+
+
+
+
+
 package br.sptrans.scd.channel.adapter.port.out.jpa.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.Mappings;
+import org.mapstruct.ReportingPolicy;
 
 import br.sptrans.scd.channel.domain.ContactChannel;
 import br.sptrans.scd.channel.adapter.port.out.jpa.entity.ContactChannelEntityJpa;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ContactChannelMapper {
-    ContactChannelMapper INSTANCE = Mappers.getMapper(ContactChannelMapper.class);
 
-    @Mapping(target = "codContato", source = "codContato")
-    // Adicione outros mapeamentos conforme necessário
+
+    // Mapeamento direto dos campos, sem propriedades inexistentes
+    @Mappings({
+        @Mapping(target = "idUsuarioCadastro", ignore = true),
+        @Mapping(target = "idUsuarioManutencao", ignore = true),
+        @Mapping(target = "codCanal", ignore = true)
+    })
     ContactChannel toDomain(ContactChannelEntityJpa entity);
 
-    @Mapping(target = "codContato", source = "codContato")
-    // Adicione outros mapeamentos conforme necessário
+    /**
+     * Converte ContactChannel para entidade ContactChannelEntityJpa (novo)
+     */
+    @Mappings({
+        @Mapping(target = "codCanal", ignore = true),
+        @Mapping(target = "idUsuarioCadastro", ignore = true),
+        @Mapping(target = "idUsuarioManutencao", ignore = true)
+    })
     ContactChannelEntityJpa toEntity(ContactChannel domain);
+
+    /**
+     * Atualiza uma entidade ContactChannelEntityJpa existente a partir de ContactChannel
+     */
+    @Mappings({
+        @Mapping(target = "codCanal", ignore = true),
+        @Mapping(target = "idUsuarioCadastro", ignore = true),
+        @Mapping(target = "idUsuarioManutencao", ignore = true),
+        @Mapping(target = "codContato", ignore = true)
+    })
+    void updateFromDomain(ContactChannel domain, @MappingTarget ContactChannelEntityJpa entity);
 }
