1e8b35e1687dab2ea08818f81851eee164d4644d
robertocordeirosptrans-web <robertocordeiro.sptrans@gmail.com>
2026-03-23 14:41:01 -0300

    feat: Adição novos campos de Tecnologias

--- src/main/java/br/sptrans/scd/product/adapter/out/jpa/entity/TechnologyEntityJpa.java
+++ src/main/java/br/sptrans/scd/product/adapter/out/jpa/entity/TechnologyEntityJpa.java
@@ -1,5 +1,8 @@
 package br.sptrans.scd.product.adapter.out.jpa.entity;
 
+import java.time.LocalDateTime;
+
+import br.sptrans.scd.auth.domain.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -28,8 +31,14 @@ public class TechnologyEntityJpa {
     private String codStatus;
 
     @Column(name = "DT_CADASTRO")
-    private String dtCadastro;
+    private LocalDateTime dtCadastro;
 
     @Column(name = "DT_MANUTENCAO")
-    private String dtManutencao;
+    private LocalDateTime dtManutencao;
+
+    @Column(name = "ID_USUARIO_CADASTRO")
+    private User idUsuarioCadastro;
+
+    @Column(name = "ID_USUARIO_MANUTENCAO")
+    private User idUsuarioManutencao;
 }
