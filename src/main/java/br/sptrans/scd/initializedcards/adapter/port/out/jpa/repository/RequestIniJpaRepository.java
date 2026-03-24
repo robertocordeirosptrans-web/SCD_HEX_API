42af0d9d5eddfa8c53c68d45856f907dd3e923a6
robertocordeirosptrans-web <robertocordeiro.sptrans@gmail.com>
2026-03-24 09:57:19 -0300

    feat: Refactor RequestInitializedAdapter to use repository and mapper for data handling

--- src/main/java/br/sptrans/scd/initializedcards/adapter/port/out/jpa/repository/RequestIniJpaRepository.java
+++ src/main/java/br/sptrans/scd/initializedcards/adapter/port/out/jpa/repository/RequestIniJpaRepository.java
@@ -6,6 +6,15 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import br.sptrans.scd.initializedcards.adapter.port.out.jpa.entity.RICEntityJpa;
 import br.sptrans.scd.initializedcards.adapter.port.out.jpa.entity.RICEntityJpaKey;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.Optional;
+
 public interface RequestIniJpaRepository extends JpaRepository<RICEntityJpa, RICEntityJpaKey>, JpaSpecificationExecutor<RICEntityJpa> {
-    
+
+	@Query("SELECT r FROM RICEntityJpa r WHERE r.id.codCanal = :codCanal AND r.id.nrSolicitacao = :nrSolicitacao")
+	Optional<RICEntityJpa> findByCodCanalAndNrSolicitacao(@Param("codCanal") String codCanal, @Param("nrSolicitacao") Long nrSolicitacao);
+
+	@Query("SELECT r FROM RICEntityJpa r WHERE r.id.codCanal = :codCanal AND r.id.nrSolicitacao = :nrSolicitacao AND r.codAdquirente = :codAdquirente")
+	Optional<RICEntityJpa> findByCodCanalAndNrSolicitacaoAndCodAdquirente(@Param("codCanal") String codCanal, @Param("nrSolicitacao") Long nrSolicitacao, @Param("codAdquirente") String codAdquirente);
 }
